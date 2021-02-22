35ad641d173242e50e34a44bc28dc2da2a7b6d3e
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-02-22 19:43:17 +0200

    css added

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-12323
+
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+    }
+    
+}
+
+// const days = Math.floor(time / (1000 * 60 * 60 * 24));
+// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+// const secs = Math.floor((time % (1000 * 60)) / 1000);
+const options = {
+    selector: 'myTimer', targetDate: new Date('Jul 17, 2021'),
+}
+
+const myTimer = new CountdownTimer(options);
+console.log(myTimer);
